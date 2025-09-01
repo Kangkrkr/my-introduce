f4c369e981c7cc29bb32f52b6eddad7c35cb6bc7
강승윤 <kangkrkr@naver.com>
2025-09-01 17:34:23 +0900

    Header 에 대한 타입 정의 및 헤더 컴포넌트 수정

--- src/components/layout/Header.vue
+++ src/components/layout/Header.vue
@@ -1,13 +1,111 @@
 <template>
-  <v-app-bar app color="rgba(255, 255, 255, 0.8)">
-    <v-toolbar-title class="font-weight-bold">KANGKRKR</v-toolbar-title>
+  <v-app-bar app color="rgba(255, 255, 255, 0.8)" flat>
+    <v-toolbar-title :class="titleMenu.class">{{ titleMenu.title }}</v-toolbar-title>
     <v-spacer></v-spacer>
-    <v-btn text href="#introduce">Introduce</v-btn>
-    <v-btn text href="#skills">Skills</v-btn>
-    <v-btn text href="#projects">Projects</v-btn>
-    <v-btn text href="#career">Career</v-btn>
+    <div class="d-none d-md-flex">
+
+      <template v-for="menu in mainMenus" :key="menu.title">
+
+        <!-- 서브 메뉴가 존재하는 경우만 메뉴 및 목록 형태로 노출 -->
+        <v-menu v-if="menu.subMenus && menu.subMenus.length > 0" open-on-hover>
+          
+          <template v-slot:activator="{ props }">
+            <v-btn v-bind="props" :href="menu.link" class="mx-1">
+              {{ menu.title }}
+              <v-icon right>mdi-chevron-down</v-icon>
+            </v-btn>
+          </template>
+
+          <v-list>
+            <template v-for="subMenu in menu.subMenus" :key="subMenu.title">
+              <v-list-item :href="subMenu.link">
+                <v-list-item-title>{{ subMenu.title }}</v-list-item-title>
+              </v-list-item>
+            </template>
+          </v-list>
+        </v-menu>
+
+        <!-- 서브 메뉴가 없는 경우, 기본 메뉴 버튼 -->
+        <v-btn v-else :href="menu.link" class="mx-1">
+          {{ menu.title }}
+        </v-btn>
+
+      </template>
+
+    </div>
+    <v-app-bar-nav-icon class="d-md-none" @click="drawer = !drawer"></v-app-bar-nav-icon>
   </v-app-bar>
+
+  <!-- 모바일용 네비게이션 드로워 -->
+  <v-navigation-drawer v-model="drawer" temporary app class="d-md-none">
+    <v-list>
+      <template v-for="menu in mainMenus" :key="menu.title">
+        <v-list-group v-if="menu.subMenus && menu.subMenus.length > 0">
+          <template v-slot:activator="{ props }">
+            <v-list-item v-bind="props" :title="menu.title"></v-list-item>
+          </template>
+          <v-list-item
+            v-for="(subMenu, i) in menu.subMenus"
+            :key="i"
+            :title="subMenu.title"
+            :href="subMenu.link"
+          ></v-list-item>
+        </v-list-group>
+        <v-list-item v-else :title="menu.title" :href="menu.link"></v-list-item>
+      </template>
+    </v-list>
+  </v-navigation-drawer>
 </template>
 
 <script setup lang="ts">
+import { onMounted, ref } from 'vue'
+import type { MainMenu, TitleMenu } from '@/types/portfolio/Header'
+
+const titleMenu = ref<TitleMenu>({
+  title: 'KANGKRKR',
+  class: 'font-weight-bold'
+})
+
+const mainMenus = ref<MainMenu[]>([])
+const drawer = ref(false)
+
+onMounted(() => {
+  mainMenus.value = [
+    {
+      title: 'Introduce',
+      link: '#introduce',
+      subMenus: [
+        {
+          title: 'Who am I?',
+          link: '#introduce'
+        },
+        {
+          title: 'Contact',
+          link: '#introduce'
+        },
+      ]
+    },
+    {
+      title: 'Skills',
+      link: '#skills'
+    },
+    {
+      title: 'Projects',
+      link: '#projects'
+    },
+    {
+      title: 'Career',
+      link: '#career'
+    }
+  ]
+})
 </script>
+
+<style scoped>
+.v-btn {
+  transition: background-color 0.3s ease;
+}
+.v-btn:hover {
+  background-color: rgba(0, 0, 0, 0.05);
+}
+</style>
